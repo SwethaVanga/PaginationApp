c977de93fae53489f3e322a3edf444617af80d7e
Swetha Sangam <swetha.sangam@gmail.com>
2021-01-17 18:59:28 +0000

    Dependencies installed

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 12,
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ],
+    'no-var': 1,
+    'eqeqeq': 1
+  }
+}
---
+++ src/components/Container.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {
+  Container as BootstrapContainer
+} from 'react-bootstrap'
+
+export const Container = () => {
+
+  return (
+    <h1 className='container__heading'>
+        Books app
+    </h1>
+  )
+
+}
